97871c1787de64c08ebeca2c992616a305a3ea18
lovevice <kazemzidane@gmail.com>
2022-06-20 00:45:45 +0400

    added main tests

--- main.cpp
+++ main.cpp
@@ -7,79 +7,103 @@
 #include "map.hpp"
 #include "stack.hpp"
 #include "vector.hpp"
-#include <list>
 #include <ctime>
 
-#define T1 int
-#define T2 std::string
+#include <stdlib.h>
 
-template <typename T>
-std::string	printPair(const T &iterator, bool nl = true, std::ostream &o = std::cout)
+#define MAX_RAM 4294967299
+#define BUFFER_SIZE 4096
+struct Buffer
 {
-	o << "key: " << iterator->first << " | value: " << iterator->second;
-	if (nl)
-		o << std::endl;
-	return ("");
-}
-
-template <typename T_MAP>
-void	printSize(T_MAP const &mp, bool print_content = 1)
-{
-	std::cout << "size: " << mp.size() << std::endl;
-	std::cout << "max_size: " << mp.max_size() << std::endl;
-	if (print_content)
-	{
-		typename T_MAP::const_iterator it = mp.begin(), ite = mp.end();
-		std::cout << std::endl << "Content is:" << std::endl;
-		for (; it != ite; ++it)
-			std::cout << "- " << printPair(it, false) << std::endl;
-	}
-	std::cout << "###############################################" << std::endl;
-}
-
-template <typename T>
-struct ft_more {
-	bool	operator()(const T &first, const T &second) const {
-		return (first > second);
-	}
+	int idx;
+	char buff[BUFFER_SIZE];
 };
 
-#define T1 int
-#define T2 std::string
-typedef ft::pair<const T1, T2> T3;
 
-static int iter = 0;
+#define COUNT (MAX_RAM / (int)sizeof(Buffer))
 
-template <typename MAP, typename U>
-void	ft_erase(MAP &mp, U param)
-{
-	std::cout << "\t-- [" << iter++ << "] --" << std::endl;
-	std::cout << "ret: " << mp.erase(param) << std::endl;
-	printSize(mp);
-}
+int main(int argc, char** argv) {
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./test seed" << std::endl;
+		std::cerr << "Provide a seed please" << std::endl;
+		std::cerr << "Count value:" << COUNT << std::endl;
+		return 1;
+	}
+	const int seed = atoi(argv[1]);
+	srand(seed);
 
-int		main(void)
-{
-	std::list<T3> lst;
-	unsigned int lst_size = 6;
-	for (unsigned int i = 0; i < lst_size; ++i)
-		lst.push_back(T3(i, std::string((lst_size - i), i + 65)));
-	ft::map<T1, T2, ft_more<T1> > mp(lst.begin(), lst.end());
-	printSize(mp);
+	TESTED_CONT::vector<std::string> vector_str;
+	TESTED_CONT::vector<int> vector_int;
+	TESTED_CONT::stack<int> stack_int;
+	TESTED_CONT::vector<Buffer> vector_buffer;
+	TESTED_CONT::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
+	TESTED_CONT::map<int, int> map_int;
+	time_t start, end;
 
-	for (int i = 2; i < 4; ++i)
-		ft_erase(mp, i);
+	time(&start);
 
-	ft_erase(mp, mp.begin()->first);
-	ft_erase(mp, (--mp.end())->first);
+	for (int i = 0; i < COUNT; i++)
+	{
+		vector_buffer.push_back(Buffer());
+	}
 
-	mp[-1] = "Hello";
-	mp[10] = "Hi there";
-	mp[10] = "Hi there";
-	printSize(mp);
+	time(&end);
 
-	ft_erase(mp, 0);
-	ft_erase(mp, 1);
+	std::cout << "It took " << end - start << " seconds to push back " << COUNT << " buffers in vector" << std::endl;
 
+
+	time(&start);
+
+	for (int i = 0; i < COUNT; i++)
+	{
+		const int idx = rand() % COUNT;
+		vector_buffer[idx].idx = 5;
+	}
+	time(&end);
+	std::cout << "It took " << end - start << " seconds to use vectors [](index) operator " << COUNT << " times" << std::endl;
+	TESTED_CONT::vector<Buffer>().swap(vector_buffer);
+
+	try
+	{
+		for (int i = 0; i < COUNT; i++)
+		{
+			const int idx = rand() % COUNT;
+			vector_buffer.at(idx);
+			std::cerr << "Error: THIS VECTOR SHOULD BE EMPTY!!" <<std::endl;
+		}
+	}
+	catch(const std::exception& e)
+	{
+		//NORMAL ! :P
+	}
+	time(&start);
+
+	for (int i = 0; i < COUNT; ++i)
+	{
+		map_int.insert(TESTED_CONT::make_pair(rand(), rand()));
+	}
+	time(&end);
+	TESTED_CONT::map<int, int> test;
+	std::cout << "It took " << end - start << " seconds to insert " << COUNT << " pairs in map" << std::endl;
+
+	time(&start);
+
+	int sum = 0;
+	for (int i = 0; i < 10000; i++)
+	{
+		int access = rand();
+		sum += map_int[access];
+	}
+	time(&end);
+	std::cout << "It took " << end - start << " seconds to access or add " << 10000 << " pairs in map" << std::endl;
+	std::cout << "should be constant with the same seed: " << sum << std::endl;
+
+	time(&start);
+	{
+		TESTED_CONT::map<int, int> copy = map_int;
+	}
+	time(&end);
+	std::cout << "It took " << end - start << " seconds to assign " << map_int.size() << " pairs to new copy map" << std::endl;
 	return (0);
 }
